7e2a6546643419c569f6ae5ae6d7dddb0cd06e9f
Anne Louise Seekford <71660299+alseekford@users.noreply.github.com>
2022-07-12 19:17:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='DS5100-M14_Package_Continued!',
+    version='1.1',
+    author='Anne Louise Seekford',
+    author_email='bng3be@virginia.edu',
+    packages=['info.py'],
+    url = 'https://github.com/alseekford/M14_Challenge',
+    license='LICENSE.txt',
+    description='Package created for UVA MSDS DS5100 Module 14 In-Class Exercise.',
+    long_description=open('README.md').read(),
+)
